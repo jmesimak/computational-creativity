b09887482ae371220c49bb78368b2a04ec509ce6
Jerry Mesimäki <jerry.mesimaki@solinor.com>
2015-09-08 08:24:39 +0300

    .gitignore is now working

--- Week 1/tokenizer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from __future__ import division
-import nltk, re, pprint
-from nltk import word_tokenize
-
-def split_text(text):
-    return word_tokenize(text)
